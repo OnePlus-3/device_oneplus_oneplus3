f274772009877dbbde4810c025504a31d4160ed4
Gaurav241 <44023468+Gaurav241@users.noreply.github.com>
2022-04-27 23:35:32 +0530

    op3: Build Radio 1.1

--- device.mk
+++ device.mk
@@ -481,6 +481,8 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.1 \
+    android.hardware.radio@1.1.vendor \
     android.hardware.radio@1.4 \
     android.hardware.radio@1.4.vendor \
     android.hardware.radio.config@1.0 \
