5e95184279b72d3953bce49899b38a1f5e2f63b1
Gaurav241 <44023468+Gaurav241@users.noreply.github.com>
2022-04-30 19:35:12 +0530

    op3: Remove Doze for time being

--- device.mk
+++ device.mk
@@ -255,8 +255,8 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
 
 # Doze mode
-PRODUCT_PACKAGES += \
-    OneplusDoze
+#PRODUCT_PACKAGES += \
+#    OneplusDoze
 
 # DRM
 PRODUCT_PACKAGES += \
